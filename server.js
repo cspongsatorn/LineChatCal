b38dc51fc3894d36ab1f49bd6990cca0c98171f2
LOOKON <ohhotor@gmail.com>
2025-08-18 14:15:28 +0700

    commit O1

--- server.js
+++ server.js
@@ -173,7 +173,7 @@ app.post('/webhook', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 10000 || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
